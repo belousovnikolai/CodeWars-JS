92fad4627a2f82ef7147925b150dea270e026acd
belousov <belousov.nikolai@gmail.com>
2020-04-15 23:34:44 +0300

    empty commit

--- src/7kyu/alternateCase.js
+++ src/7kyu/alternateCase.js
@@ -1,6 +1,6 @@
 function alternateCase(s) {
     let reversed = "";
-
+    
     for (let letter of s) {
         if (letter === letter.toUpperCase()) {
             reversed += letter.toLowerCase();
@@ -10,7 +10,6 @@ function alternateCase(s) {
             reversed += letter;
         }
     }
-
     return reversed;
 }
 
