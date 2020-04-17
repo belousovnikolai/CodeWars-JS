38e0bd3627e6f0272b650a87873cb157e9dc8828
belousov <belousov.nikolai@gmail.com>
2020-04-18 02:04:10 +0300

    Added final fixes to tests

--- src/6kyu/differenceOfTwo.js
+++ src/6kyu/differenceOfTwo.js
@@ -1,17 +1,15 @@
 function twosDifference(input) {
     const result = [];
-    input.sort((a, b) => a - b);
+
     for (let i = 0; i < input.length; i += 1) {
-        for (let j = 0; j < input.length; i += 1) {
-            if (input[i] === input[j] + 2) {
-                console.log(i, j)
+        for (let j = 0; j < input.length; j += 1) {
+            const tmp = input[i] + 2;
+            if (input[j] === tmp) {
                 result.push([input[i], input[j]]);
             }
         }
     }
-    console.log(result)
     return result;
 }
 
-twosDifference([1, 2, 3, 4]);
 module.exports = twosDifference;
