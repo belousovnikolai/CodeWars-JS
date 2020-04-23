b078064fe1eb067fa157ddf1961f07bb650304fc
belousov <belousov.nikolai@gmail.com>
2020-04-24 01:18:47 +0300

    Added solution for iNeedMoreSpeed kata.

---
+++ src/6kyu/iNeedMoreSpeed.js
@@ -0,0 +1,15 @@
+// In the initial task it is requested just change the passed array without returning from function,
+// here behavior changed for testing purpose
+
+function reverse(arr) {
+    const middle = arr.length / 2;
+
+    for (let i = 0; i < middle; i += 1) {
+        const tmp = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = tmp;
+    }
+    return arr;
+}
+
+module.exports = reverse;
