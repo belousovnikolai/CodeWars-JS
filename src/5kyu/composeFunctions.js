6c193e5b349eff295150c448f638bdf824cc54dd
belousov <belousov.nikolai@gmail.com>
2020-04-26 02:11:21 +0300

    Added solution for 'Compose function' kata

---
+++ src/5kyu/composeFunctions.js
@@ -0,0 +1,10 @@
+function compose(...args) {
+    let val = args[0];
+
+    for (let i = 1; i < args.length; i += 1) {
+        val = args[i](val);
+    }
+    return val;
+}
+
+module.exports = compose;
