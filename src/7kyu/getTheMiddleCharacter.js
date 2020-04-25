a91b2a3813e4763fe0eb7c80d062695d2249eafc
belousov <belousov.nikolai@gmail.com>
2020-04-26 01:39:21 +0300

    Added solution for 'Get the middle character' kata

---
+++ src/7kyu/getTheMiddleCharacter.js
@@ -0,0 +1,10 @@
+function getMiddle(s) {
+    const len = s.length;
+    
+    if (len % 2 !== 0) {
+        return s.substring(len / 2, len / 2 + 1);
+    }
+    return s.substring(len / 2 - 1, len / 2 + 1);
+}
+
+module.exports = getMiddle;
