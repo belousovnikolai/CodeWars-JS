5ea776c5729a8cdae9f5d8bdc2d0ceda9a52cecb
belousov <belousov.nikolai@gmail.com>
2020-04-29 17:21:57 +0300

    Cleared test artifacts from functionCache

--- src/5kyu/functionCache.js
+++ src/5kyu/functionCache.js
@@ -1,5 +1,6 @@
 function cache(func) {
     const hash = {};
+    
     return (val1, val2) => {
         const key = val1 + val2;
         if (key in hash) {
@@ -12,14 +13,4 @@ function cache(func) {
     };
 }
 
-function sample(a, b) {
-    return a + b;
-}
-
-const cachedFunction = cache(sample);
-
-console.log(cachedFunction('foo', 'bar')); // complex function should be executed
-console.log(cachedFunction('foo', 'bar')); // complex function should not be invoked again, instead the cached result should be returned
-console.log(cachedFunction('foo', 'baz')); // should be executed, because the method wasn't invoked before with these arguments
-
-// module.exports = cache;
+module.exports = cache;
