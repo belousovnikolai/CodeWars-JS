218de77dc553b79a88d48e38e5234b51ca1a6c14
belousov <belousov.nikolai@gmail.com>
2020-05-06 02:24:08 +0300

    Added solution for 'Alternate Capitalization' kata

---
+++ src/7kyu/alternateCapitalization.js
@@ -0,0 +1,18 @@
+function capitalize(s) {
+    const letters = s.split("");
+    let l = "";
+    let r = "";
+
+    for (let i = 0; i < letters.length; i++) {
+        if (i % 2 === 0) {
+            l += letters[i].toUpperCase();
+            r += letters[i].toLowerCase();
+        } else {
+            l += letters[i].toLowerCase();
+            r += letters[i].toUpperCase();
+        }
+    }
+    return [l, r];
+}
+
+module.exports = capitalize;
