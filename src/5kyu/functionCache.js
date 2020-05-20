ee566b9356045972eb2432b62671fa8befc2fbbe
belousov <belousov.nikolai@gmail.com>
2020-05-21 02:03:45 +0300

    Fixed indents in src/5kyu/functionCache.js

--- src/5kyu/functionCache.js
+++ src/5kyu/functionCache.js
@@ -1,13 +1,13 @@
 function cache(func) {
-    const c = {};
+  const c = {};
 
-    return function memo(...args) {
-        const key = JSON.stringify(args);
+  return function memo(...args) {
+    const key = JSON.stringify(args);
 
-        if (key in c) return c[key];
-        c[key] = func(...args);
-        return c[key];
-    };
+    if (key in c) return c[key];
+    c[key] = func(...args);
+    return c[key];
+  };
 }
 
 module.exports = cache;
