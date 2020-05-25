5a5608765539c0810993125c583cf821c7808b1b
belousov <belousov.nikolai@gmail.com>
2020-05-26 01:02:31 +0300

    Added solution for 'Custom Fizz Buzz array' kata

---
+++ src/6kyu/customFizzBuzzArray.js
@@ -0,0 +1,17 @@
+const fizzBuzzCustom = (stringOne = "Fizz", stringTwo = "Buzz", numOne = 3, numTwo = 5) => {
+  const sequence = [];
+  for (let i = 1; i <= 100; i++) {
+    if (i % numOne === 0 && i % numTwo === 0) {
+      sequence.push(stringOne + stringTwo);
+    } else if (i % numOne === 0) {
+      sequence.push(stringTwo);
+    } else if (i % numTwo === 0) {
+      sequence.push(stringOne);
+    } else {
+      sequence.push(i);
+    }
+  }
+  return sequence;
+};
+
+module.exports = fizzBuzzCustom;
