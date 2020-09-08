b73038f1b5a2dcf31cf34aba1b309b6b15e353e1
belousov <belousov.nikolai@gmail.com>
2020-09-08 18:01:43 +0300

    Added solution for 'split by value' kata

---
+++ src/7kyu/splitByValue.js
@@ -0,0 +1,16 @@
+function splitByValue(k, elements) {
+  const lower = [];
+  const bigger = [];
+
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    if (element < k) {
+      lower.push(element);
+    } else {
+      bigger.push(element);
+    }
+  }
+  return lower.concat(bigger);
+}
+
+module.exports = splitByValue;
