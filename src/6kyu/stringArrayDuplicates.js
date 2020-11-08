853207902c314143fd010aa707fc221d979c9615
belousov <belousov.nikolai@gmail.com>
2020-11-08 21:15:16 +0200

    Added latest kata solutions

---
+++ src/6kyu/stringArrayDuplicates.js
@@ -0,0 +1,18 @@
+function removeDuplicates(word) {
+  let previous = "";
+  let res = "";
+  for (let i = 0; i < word.length; i++) {
+    const char = word.charAt(i);
+    if (char !== previous) {
+      res += char;
+      previous = char;
+    }
+  }
+  return res;
+}
+
+function dup(s) {
+  return s.map((str) => removeDuplicates(str));
+}
+
+module.exports = dup;
---
+++ src/7kyu/peakArrayIndex.js
@@ -0,0 +1,23 @@
+function sumOfArray(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+function peak(arr) {
+  const given = [...arr];
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    const num = given.shift();
+    if (sum === sumOfArray(given)) {
+      return i;
+    }
+    sum += num;
+  }
+  return -1;
+}
+
+module.exports = peak;
