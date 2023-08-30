b1d60f2b74489e1d7a0535176280f54f0843c99e
bilousovmykola <belousov.nikolai@gmail.com>
2023-08-30 16:56:54 +0300

    Removed exports from 5 kata solutions

--- src/5kyu/composeFunctions.js
+++ src/5kyu/composeFunctions.js
@@ -1,10 +1,8 @@
 function compose(...args) {
-    let val = args[0];
+  let val = args[0];
 
-    for (let i = 1; i < args.length; i += 1) {
-        val = args[i](val);
-    }
-    return val;
+  for (let i = 1; i < args.length; i += 1) {
+    val = args[i](val);
+  }
+  return val;
 }
-
-module.exports = compose;
--- src/5kyu/functionCache.js
+++ src/5kyu/functionCache.js
@@ -9,5 +9,3 @@ function cache(func) {
     return c[key];
   };
 }
-
-module.exports = cache;
